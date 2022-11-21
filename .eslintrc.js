143ec815e970320e9b2ab73b9473509dc3b1334c
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-11-20 22:36:03 -0500

    Fix warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
       },
     ],
     "@typescript-eslint/no-unsafe-call": "off",
-    "@typescript-eslint/no-unsafe-member-acces": "off"
+    "@typescript-eslint/no-unsafe-member-acces": "off",
+    "@typescript-eslint/no-explicit-any": "off"
   },
 }
